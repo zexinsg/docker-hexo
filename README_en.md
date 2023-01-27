cdb46c7e1863d08c17467bacaf7a6ae831b7c92d
klaus <faberbeta@gmail.com>
2023-01-27 08:28:51 +0000

    commit

--- README_en.md
+++ README_en.md
@@ -1,11 +1,14 @@
 # docker-hexo
 <p align="center"> <a title="中文简体" href="README.md">🇨🇳 中文简体  </a>|  <a title="English" href="README_en.md">🇬🇧 English</a></p>
+
 How to install
 1. install docker and docker compose
-2. copy git code in your personal folder , for example "hexo"
+2. copy git code in your personal folder 
+
 ```
 git clone https://github.com/zexinsg/docker-hexo.git
 cd docker-hexo
-docker network create docker_default
 bash restart.sh
 ```
+
+Theme Git address: https://github.com/fluid-dev/hexo-theme-fluid/blob/master/README_en.md 
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -11,7 +11,10 @@ if [ "`ls -A $WORKDIR`" = "" ]; then
     npm install --save hexo-generator-searchdb \
         hexo-word-counter \
         hexo-generator-sitemap \
+	hexo-generator-baidu-sitemap \
+	hexo-generator-index \
         hexo-server
+    echo "hexo-generator-index 用于文章置顶功能 Front-matter 中配置 sticky 属性,可以让文章首页前排显示,sticky 数值越大，该文章越靠前"
     #echo ">>>>>>>>>>> install next theme"
     #git clone https://github.com/next-theme/hexo-theme-next.git themes/next
     #sed -i 's/theme: landscape/theme: next/g' _config.yml
@@ -21,9 +24,15 @@ if [ "`ls -A $WORKDIR`" = "" ]; then
     sed -i 's/theme: landscape/theme: fluid/g' _config.yml
     #fluid推荐使用，_config.fluid.yml 作为配置文件修改
     cp  _config.yml _config.fluid.yml
+    #post的文章模板修改，添加categories
+    sed -i '/^tags:.*/a\categories:' scaffolds/post.md
     #添加about页面
     hexo new page about
     sed -i '/title:/a\layout: about' source/about/index.md
+    # post_asset_folder: 设置为true, 创建目录的同事会创建一个同名的asset目录
+    sed -i '/^post_asset_folder.*/c\post_asset_folder: true' _config.fluid.yml
+    #关闭fluid主题的默认代码line number
+    sed -i 's/line_number: true/line_number: false/' node_modules/hexo-theme-fluid/_config.yml
 else
     echo ">>>>>>>>>> existed "
 fi
